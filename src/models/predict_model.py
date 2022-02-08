56d9a637baf1fea14340aa4647ac6196d44bd765
simon <simon.goldhofer@gmail.com>
2022-02-08 13:45:25 +0100

    streaming-stuff

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -10,18 +10,18 @@ cmap = plt.get_cmap('tab20b')
 colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
 font = cv.FONT_HERSHEY_SIMPLEX
 
-yolo_v5n = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+yolo_v5n = torch.hub.load('ultralytics/yolov5', 'yolov5n', pretrained=True)
 
 mot_tracker = Sort()
 
 points=[]
 
-cap = cv.VideoCapture(0)
+cap = cv.VideoCapture(3)
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
 while True:
-    if len(points)>50:
+    if len(points)>200:
         points=[]
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -37,24 +37,24 @@ while True:
         trackers = mot_tracker.update(dets)
     else:
 
-        tracked_objects = mot_tracker.update(results.pandas().xyxy[0].values[:,0:5])
+        #tracked_objects = mot_tracker.update(results.pandas().xyxy[0].values[:,0:5])
+        tracked_objects = mot_tracker.update(results.pandas().xyxy[0].values[:,0:5][np.where(results.pandas().xyxy[0].name=="person")[0],0:5])
 
-        classes=results.pandas().xyxy[0].values[:,-1]
+        classes=results.pandas().xyxy[0].values[np.where(results.pandas().xyxy[0].name=="person")[0],-1]
         for idx, track_out in enumerate(tracked_objects):
             x1, y1, x2, y2, obj_id = track_out
             x1 = int(x1)
             x2 = int(x2)
             y1 = int(y1)
             y2 = int(y2)
-            if classes[idx] == "person":
-                points.append([x1+(x2-x1)/2,y1+(y2-y1)/2]) 
-                cv.polylines(frame, np.int32([points]), 1, (255,255,255))
+            points.append([x1+(x2-x1)/2,y1+(y2-y1)/2]) 
+            cv.polylines(frame, np.int32([points]), 2, (0,255,255))
 
             frame = cv.rectangle(frame,(x1,y1),(x1+x2,y1+y2),(0,255,0),2)
             color = colors[int(obj_id) % len(colors)]
             color = [i * 255 for i in color]
-            cv.putText(frame,classes[idx] ,(x1+3,y1+25),0,0.5,(0,255,0))
-            cv.putText(frame,"ClassID: " + str(obj_id),(x1+3,y1+10),0,0.5,(0,255,0))
+            cv.putText(frame,classes[idx] ,(x1+3,y1+25),0,0.5,(0,0,0))
+            cv.putText(frame,"ObjID: " + str(obj_id)[0:5],(x1+3,y1+10),0,0.5,(0,0,0))
                 
     cv.imshow('frame', frame)
     if cv.waitKey(1) == ord('q'):
